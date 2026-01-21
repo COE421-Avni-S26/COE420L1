2b4b0f4d275a6ea579b995a824e40fba636aaf6a
tala-rojmah <g00098289@aus.edu>
2026-01-21 23:42:06 +0400

    Add COE420L1.java

--- COE420L1.java
+++ COE420L1.java
@@ -1,9 +1,16 @@
-package COE420L1;
-
 public class COE420L1 {
 	
 	public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        featureB();
     }
+	
+	public static void featureB() {
+	    System.out.println("Feature B executes");
+	}
 
 }
+
+
+
